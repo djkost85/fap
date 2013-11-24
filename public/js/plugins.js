5fcadd7fe6800646cf2bbc6584e6bfcd0f5e7302
Rashid Shamsudinov <rash2x@gmail.com>
2013-11-24 16:23:59 +0400

    fix bug run like

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -146,15 +146,22 @@ $.fn.tagit = function(){
 $.fn.like = function(){
 	return this.each(function(){
 		var self = $(this);
-		var url = self.attr('url');
+		
+		var url = self.attr('href');
 		var count = $(".like_button_count", self);
 
 		self.on({
 			'click': function(){
-				$.getJSON(url, {}, function(data){
-					if(data.status) self.update(data.count, true);
-					else self.update(data.count);
-				})
+
+				$.ajax({
+					url: url,
+					data: { ajax: true },
+					dataType: 'json'
+					success: function(data){
+						if(data.status) self.update(data.count, true);
+						else self.update(data.count);
+					}
+				});
 
 				return false;
 			}
