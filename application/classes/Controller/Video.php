d5c53130fe7cd00860360562bac8a15406ff4442
Demyashev <daseemux@inbox.ru>
2013-12-01 17:08:59 +0400

    Длительность видео + git ignore

--- application/classes/Controller/Video.php
+++ application/classes/Controller/Video.php
@@ -2,8 +2,8 @@
 
 class Controller_Video extends Controller_Base_preDispatch
 {
-	# default index page
-	public function action_index()
+    # default index page
+    public function action_index()
     {
         $videos = new Model_Video();
         $videos = $videos->getAllVideos();
@@ -33,7 +33,7 @@ class Controller_Video extends Controller_Base_preDispatch
 
         if (!$videos) {
             $this->view['videos_error'] = true;
-            $this->view['videos_error_text'] = 'Видео не найдены';
+            $this->view['videos_error_text'] = 'В этом разделе пока пусто, нам тоже от этого очень грустно..';
         }
         else {
             $this->view['videos'] = $videos;
@@ -63,7 +63,7 @@ class Controller_Video extends Controller_Base_preDispatch
 
         if (!$videos) {
             $this->view['videos_error'] = true;
-            $this->view['videos_error_text'] = 'Видео не найдены';
+            $this->view['videos_error_text'] = 'В этом разделе пока пусто, нам тоже от этого очень грустно..';
         }
         else {
             $this->view['videos'] = $videos;
@@ -75,25 +75,238 @@ class Controller_Video extends Controller_Base_preDispatch
 
     public function action_view()
     {
+        $this->view['videos_error'] = false;
+        $this->view['videos_error_text'] = '';
+
+        # show search widget in sidebar
+        $this->template->catalog = true;
+
         $title = $this->request->param('video_title', '');
 
         $Model = new Model_Video();
         $video = $Model->getVideoByTitle($title);
 
-        $Tweak = new Model_Tweak();
+        if (!$video) {
+            $this->view['videos_error'] = true;
+            $this->view['videos_error_text'] = 'Видео не найдено';
+        }
+        else {
+            $Tweak = new Model_Tweak();
 
-        $video['date']   = nl2br($Tweak->rusDate("j M Y", strtotime( $video['date'] ) ));
-
-        $this->view['video'] = $video;
-
+            $video['date']   = nl2br($Tweak->rusDate("j M Y", strtotime( $video['date'] ) ));
+            $this->view['video'] = $video;
+            
+        }
         $this->template->content = View::factory('templates/view', $this->view);
+
+        
     }
 
     public function action_add()
     {
-        # $url = Arr::get($_GET, 'url', '');
-        $this->template->additionalClasses[] = 'mainbar_black';
-        $this->template->content = View::factory('templates/add', $this->view);
+        # show search widget in sidebar
+        $this->template->catalog = true;
+
+        if ( $this->user->id != 0) {
+            $this->template->additionalClasses[] = 'mainbar_black';
+
+            $this->view['cats']    = $this->template->cats;
+            $this->view['studios'] = $this->template->studios;
+
+            # template
+            $this->view['video']['title'] = '';
+            $this->view['video']['url'] = '';
+            $this->view['video']['preview']='';
+            $this->view['video']['actors'] = '';
+            $this->view['video']['tags']='';
+            $this->view['video']['duration']='';
+            $this->view['video']['method'] = '';
+
+            # vk api
+            $data['client_id'] = '3980223';
+            $data['client_secret'] = 'ltCymYO6LwMXUbYN2xHX';
+
+            $url = (string) Arr::get($_POST, 'url', '');
+            $url = htmlspecialchars($url);
+
+            $csrf   = (string) Arr::get($_POST, 'csrf', '');
+            $title  = (string) Arr::get($_POST, 'title', '');
+            $studio = (int)    Arr::get($_POST, 'studio', '');
+            $cat    = (int)    Arr::get($_POST, 'cat', '');
+            $actors = (string) Arr::get($_POST, 'actors', '');
+            $tags   = (string) Arr::get($_POST, 'tags', '');
+            #$ajax   = (bool) Arr::get($_POST, 'ajax', false);
+            $img_preview = (string) Arr::get($_POST, 'img_preview', '');
+            $method = (string) Arr::get($_POST, 'method', ''); #save, checkUrl
+            $duration = (string)    Arr::get($_POST, 'duration', '');
+
+            
+
+            if ( $method == 'save' && Security::check( $csrf ) ) {
+                $actors = explode(',', $actors);
+                $tags   = explode(',', $tags);
+
+                $url_title = URL::title($title);
+
+                $video = new Model_Video();
+                $status = $video->save($url, $title, $url_title, $studio, $cat, $actors, $tags, $img_preview, $duration);
+
+                if ($status) $this->redirect();
+                else die('add_error');
+            }
+
+
+
+            # user add video by url
+            if ($url) {
+
+                if ( Security::check( $csrf ) ) {
+
+                    $url_title = URL::title( $title );
+
+                    # check url
+                    $pattern_iframe = '/http(s?):\/\/vk.com\/[A-Za-z_.?]*oid=([-\d]*)?&id=([\d]*)?(&hash=[A-Za-z\d]*)?(&hd=[\d])?/';
+                    $pattern_normal = '/http(s?):\/\/vk.com\/[\S\d]*video([-\d]*)?_([\d]*)?/';
+                    
+                    $pattern_iframe = htmlspecialchars($pattern_iframe);
+                    $pattern_normal = htmlspecialchars($pattern_normal);
+
+                    $status = preg_match($pattern_iframe, $url, $matches);
+                    
+                    if ( $status ) {
+
+                        # auto form array from parameters
+                        $count  = count($matches);
+
+                        $i = 0;
+                        while ( $i < $count - 1 ) {
+                            if ( isset($matches[$i])   ) $data['url'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['ssh'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['uid'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['vid'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['hash']= preg_replace('/&amp;hash=/', '', $matches[$i]);
+                            if ( isset($matches[++$i]) ) $data['hd']  = preg_replace('/&amp;hd=/', '', $matches[$i]);
+                        }
+
+                        $user = new Model_User();
+                        $vk = new Model_VK( $data['client_id'], $data['client_secret'], $user->getToken() );
+                        
+                        $video_info = $vk->api('video.get', array(
+                            'owner_id'   => $data['uid'],
+                            'videos'     => $data['uid'].'_'.$data['vid'],
+                        ));
+
+                        # duration format
+                        $hours = (int)($video_info['response'][1]['duration'] / 3600);
+                        $minuts = (int)(($video_info['response'][1]['duration'] % 3600) / 60);
+                        $seconds = $video_info['response'][1]['duration'] % 60;
+
+                        if ( $hours ) $duration = sprintf("%02d:%02d:%02d", $hours, $minuts, $seconds);
+                        else $duration = sprintf("%02d:%02d", $minuts, $seconds);
+                
+                        $video_info['response'][1]['duration'] = $duration;
+
+                        if ( $method == 'checkUrl' ) {
+                            $video_info['response'][1]['status'] = true;
+                            $video_info['response'][1]['url'] = $url;
+                            $video_info['response'][1]['method'] = $method;
+
+                            $data = json_encode($video_info['response'][1]);
+
+                            echo $data;
+                            exit();
+                        }
+                        else {
+                            # go to /add page and put info in fields
+                            $this->view['video']['url'] = $video_info['response'][1]['player'];
+                            $this->view['video']['title'] = $video_info['response'][1]['title'];
+                            $this->view['video']['preview'] = $video_info['response'][1]['image_medium'];
+                            $this->view['video']['duration'] = $video_info['response'][1]['duration'];
+                            $this->view['video']['actors'] = $actors;
+                            $this->view['video']['tags'] = $tags;
+
+                            $this->view['video']['method'] = 'save';
+
+                            $this->template->content = View::factory('templates/add', $this->view);
+
+                            return;
+                        }
+
+                    }
+
+
+                    $status = preg_match($pattern_normal, $url, $matches);
+
+                    if ( $status ) {
+
+                        # auto form array from parameters
+                        $count  = count($matches);
+
+                        $i = 0;
+                        while ( $i < $count - 1 ) {
+                            if ( isset($matches[$i])   ) $data['url'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['ssh'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['uid'] = $matches[$i];
+                            if ( isset($matches[++$i]) ) $data['vid'] = $matches[$i];
+                        }
+
+                        $user = new Model_User();
+                        $vk = new Model_VK( $data['client_id'], $data['client_secret'], $user->getToken() );
+                        
+                        $video_info = $vk->api('video.get', array(
+                            'owner_id'   => $data['uid'],
+                            'videos'     => $data['uid'].'_'.$data['vid'],
+                        ));
+
+                        # duration format
+                        $hours = (int)($video_info['response'][1]['duration'] / 3600);
+                        $minuts = (int)(($video_info['response'][1]['duration'] % 3600) / 60);
+                        $seconds = $video_info['response'][1]['duration'] % 60;
+
+                        if ( $hours ) $duration = sprintf("%02d:%02d:%02d", $hours, $minuts, $seconds);
+                        else $duration = sprintf("%02d:%02d", $minuts, $seconds);
+
+
+                        if ( $method == 'checkUrl' ) {
+                            $video_info['response'][1]['status'] = true;
+
+                            $data = json_encode($video_info['response'][1]);
+                            echo $data;
+                            exit();
+                        }
+                        else {
+                            # go to /add page and put info in fields
+                            $this->view['video']['url'] = $video_info['response'][1]['player'];
+                            $this->view['video']['title'] = $video_info['response'][1]['title'];
+                            $this->view['video']['preview'] = $video_info['response'][1]['image_medium'];
+                            $this->view['video']['duration'] = $video_info['response'][1]['duration'];
+                            $this->view['video']['actors'] = $actors;
+                            $this->view['video']['tags'] = $tags;
+
+                            $this->view['video']['method'] = 'save';
+                            $this->template->content = View::factory('templates/add', $this->view);
+                            
+                            return;
+                        }                    
+                    }
+
+                    if (!$status && $method == 'checkUrl' ) {
+                        $video_info['response'][1]['status'] = false;
+
+                        $data = json_encode($video_info['response'][1]);
+                        echo $data;
+                        exit();
+                    }     
+
+                }
+            }
+            
+            $this->template->content = View::factory('templates/add', $this->view);
+        }
+        else {
+            $this->redirect();
+        }
+        
     }
 
 }
--- application/classes/Model/Video.php
+++ application/classes/Model/Video.php
@@ -81,15 +81,17 @@ class Model_Video extends Model
         #if (!$VideosByOneDay) {
 
             if (!$condition) {
-                $q = DB::select('id', 'title', 'url', 'actors', 'cat', 'likes', 'url_title')
+                $q = DB::select('id', 'title', 'url', 'actors', 'cat', 'likes', 'url_title', 'img_preview', 'duration', 'views')
                 ->from('videos')
+                ->order_by('date', 'desc')
                 ->execute()
                 ->as_array();
             }
             else {
-                $q = DB::select('id', 'title', 'url', 'actors', 'cat', 'likes', 'url_title')
+                $q = DB::select('id', 'title', 'url', 'actors', 'cat', 'likes', 'url_title', 'img_preview', 'duration', 'views')
                     ->from('videos')
                     ->where($condition['where'], '=', $condition['parameter'])
+                    ->order_by('date', 'desc')
                     ->execute()
                     ->as_array();
             }
@@ -113,31 +115,35 @@ class Model_Video extends Model
                             $video['cat_name'] = 'Без категории';
                         }
                     }
-                    $video['actors'] = unserialize($video['actors']);
-                    
-                    # swap actors ids to names
-                    $i = 0;
-                    foreach ($actors as $actor) {
-                        foreach ($video['actors'] as $q_actor) {
 
-                            if ($actor['id'] == $q_actor) {
-                                $video['actors'][$i] = $actor['name'];
-                                $i++;
+                    if ($video['actors']) {
+                        $video['actors'] = unserialize($video['actors']);
+                    
+                        # swap actors ids to names
+                        $i = 0;
+                        foreach ($actors as $actor) {
+                            foreach ($video['actors'] as $q_actor) {
+
+                                if ($actor['id'] == $q_actor) {
+                                    $video['actors'][$i] = $actor['name'];
+                                    $i++;
+                                }
+
                             }
-
                         }
-                    }
                     
-                    # add actors name by actors_id
-                    foreach ($actors as $actor) {
+                        # add actors name by actors_id
+                        foreach ($actors as $actor) {
 
-                        if ( $video['actors'] == $actor['id'] ) {
-                            $video['actors_name'] = $actor['name'];
+                            if ( $video['actors'] == $actor['id'] ) {
+                                $video['actors_name'] = $actor['name'];
+                            }
+                            
+                            elseif ( $video['actors'] == '0' ) {
+                                $video['actors_name'] = '';
+                            }
                         }
-                        
-                        elseif ( $video['actors'] == '0' ) {
-                            $video['actors_name'] = '';
-                        }
+
                     }
 
                     $row[$count] = $video;
@@ -163,72 +169,79 @@ class Model_Video extends Model
         else {
             $user = new Model_User();
 
-            $q = DB::select('id', 'title', 'url', 'url_title', 'actors', 'cat', 'studio', 'likes', 'users_likes', 'date', 'tags', 'views')->from('videos')->where('url_title', '=', $url_title)->execute()->as_array();
+            $q = DB::select('id', 'title', 'url', 'url_title', 'actors', 'cat', 'studio', 'likes', 'users_likes', 'date', 'tags', 'views', 'duration')->from('videos')->where('url_title', '=', $url_title)->execute()->as_array();
             $q = current($q);
 
             $add_view = DB::update('videos')->value('views', $q['views'] + 1 )->where('id', '=', $q['id'])->execute();
             
-            # add css style to like button
-            $q['user_like_it_early'] = 0;
-            if ($q['likes'] != 0) {
-                
-                $q['users_likes'] = unserialize($q['users_likes']);
+            if (!$q) return false;
+            else {
+                # add css style to like button
+                $q['user_like_it_early'] = 0;
+                if ($q['likes'] != 0 ) {
+                    
+                    $q['users_likes'] = unserialize($q['users_likes']);
 
-                foreach ($q['users_likes'] as $u) {
-                    if ( $u == $user->id ) $q['user_like_it_early'] = 1;
+                    foreach ($q['users_likes'] as $u) {
+                        if ( $u == $user->id ) $q['user_like_it_early'] = 1;
+                    }
                 }
+
+
+                $model = new Model_Video();
+                $cats    = $model->getCats();
+                $studios = $model->getStudios();
+                $actors  = $model->getActors();
+                $tags    = $model->getTags();
+
+                foreach ($cats as $cat) {
+                    if ($cat['id'] == $q['cat']) {
+                        $q['cat_name'] = $cat['name'];
+                    }
+                }
+
+                foreach ($studios as $studio) {
+                    if ($studio['id'] == $q['studio']) {
+                        $q['studio_name'] = $studio['name'];
+                    }
+                }
+
+                if ($q['actors']) {
+                    # swap actors ids to names
+                    $i = 0;
+                    $q['actors'] = unserialize($q['actors']);
+                    foreach ($q['actors'] as $act) {
+                        foreach ($actors as $actor) {
+
+                            if ($actor['id'] == $act) {
+                                $q['actors'][$i] = $actor['name'];
+                                $i++;
+                            }
+
+                        }
+                    }
+                }
+                
+                if ($q['tags']) {
+                    # swap tags ids to tags name
+                    $i = 0;
+                    $q['tags'] = unserialize($q['tags']);
+                    foreach ($q['tags'] as $q_tag) { #zapros
+                        foreach ($tags as $tag) { #massiv id name
+
+                            if ($tag['id'] == $q_tag) {
+                                $q['tags'][$i] = $tag['name'];
+                                $i++;
+                            }
+
+                        }
+                    }
+                }
+                
+                return $q;
             }
             
-           
-      
-            $model = new Model_Video();
-            $cats    = $model->getCats();
-            $studios = $model->getStudios();
-            $actors  = $model->getActors();
-            $tags    = $model->getTags();
-
-            foreach ($cats as $cat) {
-                if ($cat['id'] == $q['cat']) {
-                    $q['cat_name'] = $cat['name'];
-                }
-            }
-
-            foreach ($studios as $studio) {
-                if ($studio['id'] == $q['studio']) {
-                    $q['studio_name'] = $studio['name'];
-                }
-            }
-
-            # swap actors ids to names
-            $i = 0;
-            $q['actors'] = unserialize($q['actors']);
-            foreach ($q['actors'] as $act) {
-                foreach ($actors as $actor) {
-
-                    if ($actor['id'] == $act) {
-                        $q['actors'][$i] = $actor['name'];
-                        $i++;
-                    }
-
-                }
-            }
-
-            # swap tags ids to tags name
-            $i = 0;
-            $q['tags'] = unserialize($q['tags']);
-            foreach ($q['tags'] as $q_tag) { #zapros
-                foreach ($tags as $tag) { #massiv id name
-
-                    if ($tag['id'] == $q_tag) {
-                        $q['tags'][$i] = $tag['name'];
-                        $i++;
-                    }
-
-                }
-            }
-
-            if (!$q) return false;
-            else return $q;
+            
         }
         
     }
@@ -266,5 +279,21 @@ class Model_Video extends Model
             if (!$q) return false;
             else return $q['id'];
         }
+    }
+
+    public function save($url, $title, $url_title, $studio, $cat, $actors, $tags, $img_preview, $duration) {
+        #$actors = serialize($actors);
+        #$tags = serialize($tags);
+
+        #$model = new Model_Video();
+        #$all_tags = $model->getTags();
+        #$count = count($all_tags)
+
+        
+        $fields = array('url', 'url_title', 'img_preview', 'title', 'studio', 'cat', 'actors', 'tags', 'duration');
+        $values = array($url, $url_title, $img_preview, $title, $studio, $cat, '0', '0', $duration);
+
+        $status = DB::insert('videos', $fields)->values($values)->execute();
+        return $status;
     }
 }
